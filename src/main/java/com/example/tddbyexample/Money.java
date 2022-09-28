30b55eceff35fd4977624bd073f2e204ebaeed4b
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-09-28 07:30:34 +0300

    Adding making objects.

--- src/main/java/com/example/tddbyexample/Money.java
+++ src/main/java/com/example/tddbyexample/Money.java
@@ -1,7 +1,18 @@
 package com.example.tddbyexample;
 
-public class Money {
+public abstract class Money {
+
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object o) {
