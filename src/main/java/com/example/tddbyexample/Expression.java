b1a0c6abc9e9bdfb39e8583e8e95f3e379d4f9c4
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-09-30 06:34:44 +0300

    Adding Bank and Expression and tests.

---
+++ src/main/java/com/example/tddbyexample/Expression.java
@@ -0,0 +1,4 @@
+package com.example.tddbyexample;
+
+public interface Expression {
+}
--- src/main/java/com/example/tddbyexample/Money.java
+++ src/main/java/com/example/tddbyexample/Money.java
@@ -1,6 +1,6 @@
 package com.example.tddbyexample;
 
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -11,11 +11,11 @@ public class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     @Override
@@ -35,5 +35,9 @@ public class Money {
 
     public Money times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
+    }
+
+    public Expression plus (Money add) {
+        return new Money(amount + add.amount, currency);
     }
 }
